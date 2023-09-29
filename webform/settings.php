ff0e50b85604e9da0fa2d5fa938292737f3b66ab
minutogit <sgalek@gmx.de>
2023-09-29 23:46:26 +0200

    improvements
    Took 15 minutes

--- webform/settings.php
+++ webform/settings.php
@@ -4,8 +4,8 @@ $from_email = "myemail@domain.com"; // source mail address / Mail Absender
 $recipient = "myemail@domain.com"; // recipient of the Mail / Mail Empfaenger
 $subject = "[Regional-Vernetzt Eintrag]"; // subject / Betreff der Mail
 $subject_confirmed = "[Regional-Vernetzt Aktuallisierter Eintrag] "; // subject when the mail address is already confirmed / Betreff der Mail wenn die Mailadresse bereits bestätigt ist
-$confirmation_mail_subject = "[Regional-Vernetzt Eintrag] bestaetigt"; // mailsubject for confirmation of the entry / Betreff der Mail
-$confirmation_mail_text = "Eintrag wurde per Mail Link bestaetigt."; // mailtext for confirmation of the entry / Text der Mail
-$website_alert_confirmation_text = "Eintragung wurde bestaetigt."; // msg-box infotext when user confirms the entry
+$confirmation_mail_subject = "[Regional-Vernetzt Eintrag] bestätigt"; // mailsubject for confirmation of the entry / Betreff der Mail
+$confirmation_mail_text = "Der Eintrag wurde erfolgreich über den Mail-Link bestätigt. Damit ist die E-Mail-Adresse des Eintragers korrekt."; // mailtext for confirmation of the entry / Text der Mail
+$website_alert_confirmation_text = "Vielen Dank. Der Eintrag wurde somit bestätigt."; // msg-box infotext when user confirms the entry
 $delete_files_after_years = 5; // after how many years the encrypted files with content will be deleted
 ?>
