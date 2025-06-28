7e76f76ea323a23d61486f749beee0a5c78dddc5
Sebastian <sgalek@gmx.de>
2025-06-28 14:46:31 +0200

    Add German translations and update GitHub Actions workflow
    - Upload to Github German translation files `talent_de.qm`, `talent_de.ts`, and `QPlatformTheme_de.ts` for the application.
    - Update the GitHub Actions workflow to trigger release builds only on new tags.
    - Modify steps in the workflow to correctly handle artifact naming and simplify release creation process.
    
    Took 10 minutes

---
+++ translations/talent_de.ts
@@ -0,0 +1,243 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="38"/>
+        <location filename="../qt_gui/main_window.ui" line="268"/>
+        <source>test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="42"/>
+        <source>Spalten</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="50"/>
+        <source>Filter:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="89"/>
+        <source>Alle Gültigen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="94"/>
+        <source>Nur Eigene</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="99"/>
+        <source>Nicht Eigene</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="104"/>
+        <source>Eigene (abgelaufen)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="109"/>
+        <source>Ausgeblendete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="129"/>
+        <source>---</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="136"/>
+        <location filename="../qt_gui/main_window.ui" line="198"/>
+        <source>Visitenkarte hinzufügen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="153"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="161"/>
+        <source>Erweitert</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="170"/>
+        <source>Friends</source>
+        <translation>Freunde</translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="179"/>
+        <source>Profil</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="203"/>
+        <source>Datenbank an Freunde teilen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="208"/>
+        <source>Profil erstellen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="213"/>
+        <source>Datenbank von Freunden importieren</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="218"/>
+        <source>Datenbank mit Passwort teilen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="223"/>
+        <source>Datenbank mit Passwort importieren</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="228"/>
+        <source>Freund hinzufügen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="233"/>
+        <source>Meine Freunde</source>
+        <translation>Meine Freunde</translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="238"/>
+        <source>Passwort ändern</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="243"/>
+        <source>Profil wiederherstellen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="248"/>
+        <source>Passwort vergessen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="253"/>
+        <source>Anmelden</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="258"/>
+        <source>Abmelden</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="263"/>
+        <source>Beenden</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="273"/>
+        <source>Datenbank reinigen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="278"/>
+        <source>Profil Einstellungen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="283"/>
+        <source>Schlüsselwörter anzeigen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt_gui/main_window.ui" line="288"/>
+        <source>HTML-Export</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QPlatformTheme</name>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Speichern</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Öffnen</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Schließen</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Anwenden</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Zurücksetzen</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <source>Don&apos;t Save</source>
+        <translation>Nicht speichern</translation>
+    </message>
+    <message>
+        <source>Discard</source>
+        <translation>Verwerfen</translation>
+    </message>
+    <message>
+        <source>&amp;Yes</source>
+        <translation>&amp;Ja</translation>
+    </message>
+    <message>
+        <source>Yes to &amp;All</source>
+        <translation>Ja zu &amp;alle</translation>
+    </message>
+    <message>
+        <source>&amp;No</source>
+        <translation>&amp;Nein</translation>
+    </message>
+    <message>
+        <source>N&amp;o to All</source>
+        <translation>N&amp;ein zu alle</translation>
+    </message>
+    <message>
+        <source>Save All</source>
+        <translation>Alle speichern</translation>
+    </message>
+    <message>
+        <source>Abort</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <source>Retry</source>
+        <translation>Wiederholen</translation>
+    </message>
+    <message>
+        <source>Ignore</source>
+        <translation>Ignorieren</translation>
+    </message>
+    <message>
+        <source>Restore Defaults</source>
+        <translation>Standardeinstellungen wiederherstellen</translation>
+    </message>
+</context>
+</TS>
